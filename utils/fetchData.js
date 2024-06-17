3c2a9e6b59bde96aeaefc4f60ae43d76904a1249
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2024-06-17 13:38:12 +0530

    Update fetchData.js

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000'
+const baseUrl = 'https://evchargingwow.vercel.app'
 
 export const getData = async (url, token) => {
     const res = await fetch(`${baseUrl}/api/${url}`, {
